ffb38db5e48394de8b6b8547a2cb7e8c3dab8276
gonzalogarcia <gonzalo.garcia@intive.com>
2023-03-18 00:15:36 -0300

    admins

--- drfguitars/brands/admin.py
+++ drfguitars/brands/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 
-from .models import Brand
+from .models import Brand, Line
 
 
 class BrandConfig(admin.ModelAdmin):
     pass
 
 
+class LineConfig(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(Brand, BrandConfig)
+admin.site.register(Line, LineConfig)
